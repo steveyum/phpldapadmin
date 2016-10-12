381efbe2a1cbd2632c3c06c5b202278b16dd33d0
Stephen Yum <steveyum@gmail.com>
2016-10-12 02:40:23 -0700

    minor changes

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-sudo cp etc /etc/phpldapadmin
-sudo cp share /usr/local/share/phpldapadmin
+sudo cp -r ./etc /etc/phpldapadmin
+sudo cp -r ./share /usr/local/share/phpldapadmin
 
 sudo chown www-data:www-data /etc/phpldapadmin/
 
 sudo chmod -R 750 /etc/phpldapadmin/templates
-sudo chmod 640 /etc/phpldapadmin/template.dtd
+sudo chmod 640 /etc/phpldapadmin/templates/template.dtd
 
 if [ -d /var/www/ ]; then
   sudo ln -s /usr/local/share/phpldapadmin /var/www/ldap
