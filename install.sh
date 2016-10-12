54ac113e0c678cd5451e1d3569ac7399a834932b
Stephen Yum <steveyum@gmail.com>
2016-10-12 03:17:22 -0700

    final debugging of installer

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ sudo chown www-data:www-data /etc/phpldapadmin/
 sudo chmod -R 750 /etc/phpldapadmin/templates
 sudo chmod 640 /etc/phpldapadmin/templates/template.dtd
 
-if [ -d /var/www/ ]; then
-  sudo ln -s /usr/local/share/phpldapadmin /var/www/ldap
+if [ ! -d /var/www/ ]; then
+  sudo mkdir -p /var/www
 fi
+sudo ln -s /usr/local/share/phpldapadmin /var/www/ldap
 
 
